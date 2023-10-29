0e0783e789683da129699093f00ee7bbb7ef6cd2
HarpLoid <walesii19@gmail.com>
2023-10-29 22:17:15 +0100

    added method to get cities by state

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -14,9 +14,14 @@ from models import storage
 @app_views.route('/states/<state_id>/cities', methods=['GET'],
                  strict_slashes=False)
 def cities_by_state(state_id):
+    all_cities = storage.all(City)
+    state = storage.get("State", state_id)
     cities = []
-    states = storage.all("State").values()
 
-    for state in states:
-        for city in state.cities():
-            cities.append(city)
+    if state is None:
+        abort(404)
+
+    for city in all_cities.values():
+        if city.state_id == state_id:
+            cities.append(city.to_dict())
+    return jsonify(cities)
